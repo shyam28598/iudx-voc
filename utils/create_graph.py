da301aed35f5d68a73dc6dcc6af21cb7a6486c19
rraks <rakshitadmar@gmail.com>
2020-04-14 22:05:07 +0530

    Merged utils from dev-m

--- utils/create_graph.py
+++ utils/create_graph.py
@@ -2,23 +2,22 @@
 import json
 import os
 import glob
+import copy
 
 from collections import OrderedDict
 
 
-folder_path = "../base-schemas/classes/"
-#folder_path = "../base-schemas/properties/"
+folder_path = "../base-schemas/properties/"
 for filename in glob.glob(os.path.join(folder_path, '*.jsonld')):
     with open(filename, "r+") as obj_file:
         new_dict = OrderedDict()
+        new_list = []
         obj = json.load(obj_file)
-        new_dict["@context"] = obj["@context"]
+        template = copy.deepcopy(obj)
         del(obj["@context"]) 
-        new_dict["@graph"] = obj["@graph"]
-        for field in new_dict["@graph"]:
-            label = field["@id"].split(":")
-            tmp = field["rdfs:label"]
-            field["rdfs:label"] = label[1]
+        new_list.append(copy.deepcopy(obj))
+        new_dict["@context"] = template["@context"]
+        new_dict["@graph"] = new_list
         obj_file.seek(0)
         json.dump(new_dict, obj_file, indent=4)
         obj_file.truncate()
