db74d2c832f250b2d5ab710734efdcc2ab32f9b9
rraks <rakshitadmar@gmail.com>
2020-04-16 19:54:41 +0530

    Modified schema structures

---
+++ utils/push/push_schema.py
@@ -0,0 +1,38 @@
+import json
+import requests
+import os
+import sys
+
+dirname = sys.argv[1]
+uname = sys.argv[2]
+password = sys.argv[3]
+
+
+# local
+# url = "https://localhost:8080"
+# verify = False
+
+
+# ext
+url = "https://voc.iudx.org.in"
+verify = True
+
+headers = {"username": uname, "password": password, "content-type": "application/json+ld"}
+headers = {"username": uname, "password": password, "content-type": "application/json+ld"}
+
+
+failed_list = []
+for filename in os.listdir(dirname):
+    with open(dirname + "/" + filename, 'r') as f:
+        doc = json.load(f)
+        name = doc["@graph"][0]["@id"][5:]
+        print("Pushing " + name)
+        if (verify is True):
+            r = requests.post(url+"/"+name, data=json.dumps(doc), headers=headers)
+            if r.status_code != 201 :
+                failed_list.append(name)
+        else:
+            r = requests.post(url+"/"+name, data=json.dumps(doc), headers=headers, verify=verify)
+
+with open("failed.txt", "w") as f:
+    json.dump(failed_list, f)
